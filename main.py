2b22fad06d2ca576e507924b0d68eb23268cadbf
bintehoor <50301178+bintehoor@users.noreply.github.com>
2019-06-16 17:54:21 +0500

    even odd number with function...(class activity)
    class activity

---
+++ main.py
@@ -0,0 +1,16 @@
+num = int(input("Enter number :"))
+def even_odd(num):
+    if (num%2 == 0):
+        return ("number is even")
+
+    else:
+        return ("number is odd")
+print(even_odd(num))
+
+
+
+
+
+
+
+
